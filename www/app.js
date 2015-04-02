67681e6623ea08890a9a78cad41b7a47c9a3aa28
David Sheardown <davidsheardown@hotmail.com>
2015-04-02 12:35:04 +0100

    Testing full prod build - added EnableDeveloperConsole=false to config.xml

--- www/app.js
+++ www/app.js
@@ -1,9 +1,19 @@
 var MARGIN_SMALL = 4;
 var MARGIN = 8;
 
+var drawer = tabris.create("Drawer");
+tabris.create("PageSelector", {
+    layoutData: {left: 0, top: 0, right: 0, bottom: 0}
+}).appendTo(drawer);
+
 var page = tabris.create("Page", {
     topLevel: true,
-    title: "Fancy Food"
+    title: "WildAx Aurora"
+});
+
+var page2 = tabris.create("Page", {
+    topLevel: true,
+    title: "Another Motorhome"
 });
 
 var scrollView = tabris.create("ScrollView", {
@@ -21,32 +31,18 @@ var contentComposite = tabris.create("Composite", {
 
 tabris.create("TextView", {
     layoutData: {left: MARGIN, right: MARGIN, top: MARGIN},
-    text: "Etiam nisl nisi, egestas quis lacus ut, tristique suscipit metus. In vehicula lectus " +
-    "metus, at accumsan elit fringilla blandit. Integer et quam sed dolor pharetra " +
-    "molestie id eget dui. Donec ac libero eu lectus dapibus placerat eu a tellus. Fusce " +
-    "vulputate ac sem sit amet bibendum. Pellentesque euismod varius purus pharetra. Sed " +
-    "vitae ipsum sit amet risus vehicula euismod in at nunc. Sed in viverra arcu, id " +
-    "blandit. Praesent sagittis quis nisl id molestie. Donec dignissim, nisl id volutpat " +
-    "consectetur, massa diam aliquam lectus, sed euismod leo elit eu justo. Integer vel " +
-    "ante sapien.\n\nNunc sit amet blandit tellus, sed neque. Proin vel elementum augue. " +
-    "Quisque gravida nulla nisl, at fermentum turpis euismod in. Maecenas tortor at ante " +
-    "vulputate iaculis at vitae sem. Nulla dui erat, viverra eget mauris in, sodales " +
-    "mollis. Integer rhoncus suscipit mi in pulvinar. Nam metus augue, dictum a egestas " +
-    "ut, gravida eget ipsum. Nunc nisl, mollis et mauris in, venenatis blandit magna. " +
-    "Nullam scelerisque tellus lacus, in lobortis purus consectetur sed. Etiam pulvinar " +
-    "sapien vel nibh vehicula, in lacinia odio pharetra. Duis tincidunt metus a semper " +
-    "auctor. Sed nec consequat augue, id vulputate orci. Nunc metus nulla, luctus id " +
-    "porttitor nec, sed lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+    text:   "Our first scamper-van.  This is definitely one of the motorhomes " +
+            "we are focusing on right now!"
 }).appendTo(contentComposite);
 
 var titleComposite = tabris.create("Composite", {
     id: "titleComposite",
-    background: "rgba(255,152,0,0.9)"
+    background: "rgba(0,158,195,1)"
 }).appendTo(scrollView);
 
 tabris.create("TextView", {
     markupEnabled: true,
-    text: "<b>The perfect side dish</b>",
+    text: "<b>Easy to get around, looks good too</b>",
     font: "16px",
     layoutData: {left: MARGIN, top: MARGIN, right: MARGIN},
     foreground: "black"
@@ -55,7 +51,7 @@ tabris.create("TextView", {
 tabris.create("TextView", {
     layoutData: {left: MARGIN, bottom: MARGIN_SMALL, right: MARGIN},
     markupEnabled: true,
-    text: "<b>INDIAN SUMMER SALAD</b>",
+    text: "<b>WILDAX - Aurora</b>",
     font: "24px",
     foreground: "white"
 }).appendTo(titleComposite);
@@ -66,7 +62,7 @@ scrollView.on("change:bounds", function() {
     var pageWidth = bounds.width;
     var pageHeight = bounds.height;
     var imageHeight = pageWidth / 1.4; // 1.4 is the image aspect ratio
-    imageTextView.set("image", {src: "images/salad.jpg", width: pageWidth, height: pageWidth});
+    imageTextView.set("image", {src: "images/scampervan.jpg", width: pageWidth, height: pageWidth});
     var titleCompHeight = titleComposite.get("bounds").height;
     // we need the offset of the title composite in each scroll event
     // it can only change when a change:bounds is triggered, wo thats when we assign it
@@ -77,7 +73,7 @@ scrollView.on("change:bounds", function() {
 scrollView.on("scroll", function(offset) {
     imageTextView.set("transform", {translationY: offset.y * 0.4});
     if (titleCompY - offset.y < 0) {
-        titleComposite.set("transform", {translationY: offset.y - titleCompY});
+        titleComposite.set("transform", {translationY: offset.y - titleCompY, opacity: 3});
     } else {
         titleComposite.set("transform", {translationY: 0});
     }
